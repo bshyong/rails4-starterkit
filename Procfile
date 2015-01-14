82fba4aa24abec9c51840c2b87fdb3ec07911159
Ben <hello@benshyong.com>
2015-01-14 14:29:29 -0800

    tweak

--- Procfile
+++ Procfile
@@ -1,4 +1,7 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+redis: redis-server
+memcached: memcached -v
+guard: bundle exec guard
 
 # See config/unicorn.rb for disabling spawing of workers in web dyno
 # worker: bundle exec sidekiq -C config/sidekiq.yml
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json'}
 
   # Devise, require authenticate by default
   before_filter :authenticate_user!
